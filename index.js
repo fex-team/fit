15a4546f6a196263fc2880b8f8ee14cf944b0c36
huangziyi01 <huangziyi01@baidu.com>
2015-12-09 19:18:34 +0800

    add group

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
-import React from 'react'
-import classnames from 'classnames'
 import 'tb-style'
 
-export default class Layout extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+import ButtonComponent from './button'
+import GroupComponent from './group'
 
-    render() {
-        // addon
-        let addon = null
-        let addonValue = this.props['addon-left'] || this.props['addon-right']
-        if (addonValue) {
-            let addonClass = classnames({
-                'fa': true,
-                ['fa-' + addonValue]: true
-            })
-            addon = (
-                <i className={addonClass}></i>
-            )
-        }
+export default ButtonComponent
 
-        let btnClass = classnames({
-            'btn': true,
-            ['btn-' + this.props.type]: true,
-            'disabled': this.props.disabled,
-            'btn-addon': addonValue
-        })
-
-        return (
-            <button {...this.props} className={btnClass}>
-                {addon ? addon : null}
-                {this.props.children}
-            </button>
-        )
-    }
+export class Button extends ButtonComponent {
 }
 
-Layout.defaultProps = {
-    type: 'default',
-    disabled: false
+export class ButtonGroup extends GroupComponent {
 }
