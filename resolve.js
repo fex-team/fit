354fd3a771fe2fced4e92133f58542b5c41f50c1
huangziyi01 <huangziyi01@baidu.com>
2015-12-09 17:38:30 +0800

    update

--- resolve.js
+++ resolve.js
@@ -2,6 +2,7 @@ var path = require('path')
 
 module.exports = {
     alias: {
-        'tb-button': path.join(__dirname, 'lib/button')
+        'tb-button': path.join(__dirname, 'lib/button'),
+        'tb-style': path.join(__dirname, 'src/all.scss')
     }
 }
