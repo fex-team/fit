50b3087f1c748f065841b623654f9c09cdd88ec6
dongtiancheng <dongtiangche@gmail.com>
2015-12-10 23:57:58 +0800

    update

--- update-component.sh
+++ update-component.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 ROOT=`pwd`
+TIEBAACCOUNT="http://gitlab.baidu.com/tb-component/awesome/blob/master/doc/publish.md"
 
+
+## check first param
+if [ -z "$1" ]; then
+    echo "the first param is needed" && exit 1
+fi
+
+## npm login
 login() {
    npm login
 }
 
-log () {
-  printf "  \033[36m%10s\033[0m : \033[90m%s\033[0m\n" $1 $2
-}
 
 update() {
     cd ./lib/$1
@@ -32,12 +37,16 @@ checkChange () {
     fi
 }
 
+
 checkChange
 
 if test `npm whoami` = tieba; then
     update $1
 else
     log "You must login with tieba"
+    echo "|---------------------------------------------"
+    echo "|  Login Url:   $TIEBAACCOUNT                 "
+    echo "|---------------------------------------------"
     login
     update $1
 fi
