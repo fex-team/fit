0a9f08a6d9e9564f919c1e78ab0c411397c9d8a3
huangziyi01 <huangziyi01@baidu.com>
2015-12-12 23:16:15 +0800

    update

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 <div id='react-dom'></div>
 </body>
 
+<script src="/output/cdn/React.js"></script>
+<script src="/output/cdn/ReactDOM.js"></script>
+<script src="/output/cdn/ReactRouter.js"></script>
+<script src="/output/cdn/classNames.js"></script>
+<script src="/output/cdn/_.js"></script>
+<script src="/output/cdn/$.js"></script>
+<script src="output/index.js"></script>
 <script src="output/index.js"></script>
 
 </html>
