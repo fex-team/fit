6d758c2d52978815c4d6d8a8a01ccf20cc53dee7
huangziyi01 <huangziyi01@baidu.com>
2015-12-14 20:31:14 +0800

    quick push

--- publish.sh
+++ publish.sh
@@ -57,7 +57,6 @@ checkChange
 checkWhoami
 
 if test `npm whoami` = tieba; then
-    cp -r lib/$1/src lib/$1/dist
     babel lib/$1/src --out-dir lib/$1/dist
     update $1
     sudo rm -rf lib/$1/dist
