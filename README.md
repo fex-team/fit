479b6f668509a3a51ee1978df80e792b676001e5
dongtiancheng <dongtiancheng@baidu.com>
2015-12-18 00:58:18 +0800

    Squashed 'lib/datepicker/' changes from 7972b16..0b60938
    0b60938 upgrade package: datepicker
    d1326d6 quick push
    d3abc6f quick push
    b966940 update
    1a7484c update
    e4f1ce5 bug fixe
    45ef9dc upgrade package: datepicker
    5e07adb update
    ba98edd update
    91ce9c5 upgrade package: datepicker
    b990af1 upgrade package: datepicker
    e524942 upgrade package: datepicker
    f3c5c77 upgrade package: datepicker
    28395c7 upgrade package: datepicker
    82bbeb5 update
    effc0d7 update
    eead88d add something
    cc4e999 Merge commit 'c8512ba8be3d4df3b44766573847fa8080fe635a'
    c8512ba add package.json
    fdbfea3 modify datapicker
    
    git-subtree-dir: lib/datepicker
    git-subtree-split: 0b60938b45ad5dab563167886a1aa3aa3d6bfd6e

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 datepikcer
 ================
+
+helloworld
---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+//import React from 'react'
+//import 'tb-style'
+//
+//export default class Datepicker extends React.component {
+//    constructor (props) {
+//        super(props)
+//        this.state = {}
+//    }
+//
+//
+//}
+//
+
+//export { Datepicker } from 'antd'
