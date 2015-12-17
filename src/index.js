091b94d4b99b985af0e5fd259b8984ba985b6d72
dongtiancheng <dongtiancheng@baidu.com>
2015-12-18 00:57:19 +0800

    Squashed 'lib/checkbox/' changes from 1f6d3ca..6b75be7
    6b75be7 upgrade package: checkbox
    400b8c9 quick push
    
    git-subtree-dir: lib/checkbox
    git-subtree-split: 6b75be7e6de4cc1bab935ca24f460262bf0e027d

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import $ from 'jquery'
+
+export default class CheckBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        )
+    }
+}
