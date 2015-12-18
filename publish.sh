c22d05d420b065fec94ca8a7dd5ef8d79257986e
dongtiancheng <dongtiancheng@baidu.com>
2015-12-18 16:59:18 +0800

    quick push

--- publish.sh
+++ publish.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-exec 3>&2 # logging stream (file descriptor 3) defaults to STDERR
-verbosity=3 # default to show warnings
-silent_lvl=0
-crt_lvl=1
-err_lvl=2
-wrn_lvl=3
-dbg_lvl=4
-inf_lvl=5
-
-notify() { log $silent_lvl "NOTE: $1"; } # Always prints
-critical() { log $crt_lvl "CRITICAL: $2"; }
-error() { log $err_lvl "ERROR: $1"; }
-warn() { log $wrn_lvl "WARNING: $1"; }
-debug() { log $dbg_lvl "DEBUG: $1"; }
-inf() { log $inf_lvl "INFO: $1"; } # "info" is already a command
-log() {
-    if [ $verbosity -ge $1 ]; then
-        datestring=`date +'%Y-%m-%d %H:%M:%S'`
-        # Expand escaped characters, wrap at 70 chars, indent wrapped lines
-        echo -e "$datestring $2" | fold -w70 -s | sed '2~1s/^/  /' >&3
-    fi
-}
-
 ROOT=`pwd`
 TIEBAACCOUNT="http://gitlab.baidu.com/tb-component/awesome/blob/master/doc/publish.md"
 
@@ -36,7 +13,7 @@ function ctrl_c() {
 
 ## check first param
 if [ -z "$1" ]; then
-    error "the first param is needed" && exit 1
+    echo "the first param is needed" && exit 1
 fi
 
 ## npm login
@@ -56,20 +33,20 @@ update() {
             cd $ROOT
             git subtree pull --prefix=lib/$1 $1 master 2>/dev/null || exit 1
             git subtree push --prefix=lib/$1 $1 master 2>/dev/null
-            notify "subtree:$1 push success"
+            echo "subtree:$1 push success"
         else
-            error "There is no package.json file in `pwd`"
+            echo "There is no package.json file in `pwd`"
         fi
     else
-        error "no such file or directory `pwd`/lib/$1"
+        echo "no such file or directory `pwd`/lib/$1"
     fi
 }
 
 checkChange () {
     if git diff-index --quiet HEAD --; then
-        notify "INFO:" "there no changes :)"
+        echo "INFO:" "there no changes :)"
     else
-        error "ERROR:" "there are changes, please commit first" && exit 1
+        echo "ERROR:" "there are changes, please commit first" && exit 1
     fi
 }
 
@@ -79,7 +56,7 @@ checkWhoami () {
     npm whoami > /dev/null 2>&1 || knowami=0
 
     if [ $knowami == 0 ]; then
-        error "You are not login..\n run npm login and login with tieba \n $TIEBAACCOUNT" && exit 1
+        echo "You are not login..\n run npm login and login with tieba \n $TIEBAACCOUNT" && exit 1
     fi
 }
 
@@ -88,15 +65,15 @@ checkWhoami
 
 if test `npm whoami` = tieba; then
     node webpack.publish.js $1
-    log "pack:$1 success"
+    echo "pack:$1 success"
     update $1
     rm -rf lib/$1/dist
-    log "remove lib/$1/dist"
+    echo "remove lib/$1/dist"
 else
-    warn "You must login with tieba"
-    warn "|---------------------------------------------"
-    warn "|  Login Url:   $TIEBAACCOUNT                 "
-    warn "|---------------------------------------------"
+    echo "You must login with tieba"
+    echo "|---------------------------------------------"
+    echo "|  Login Url:   $TIEBAACCOUNT                 "
+    echo "|---------------------------------------------"
     login
     update $1
 fi
