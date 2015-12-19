d454ec7c4cc4a42d177ba8cc8c99d86a10316476
huangziyi01 <huangziyi01@baidu.com>
2015-12-19 16:29:41 +0800

    add checkbox radio

---
+++ src/radio/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import classNames from 'classnames'
+import './index.scss'
+
+export default class Radio extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            checked: this.props.checked
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if ('checked' in nextProps) {
+            this.setState({
+                checked: nextProps.checked
+            })
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            checked: event.target.checked
+        }, ()=> {
+            this.props.onChange(this.state.checked)
+        })
+    }
+
+    render() {
+        let classname = classNames({
+            '_namespace': true,
+            'i-checks': true,
+            'i-checks-lg': this.props.size === 'large',
+            'i-checks-sm': this.props.size === 'small',
+            'disabled': this.props.disabled
+        })
+
+        return (
+            <label style={this.props.style}
+                   className={classname}>
+                <input type="radio"
+                       disabled={this.props.disabled}
+                       checked={this.state.checked}
+                       onChange={this.handleChange.bind(this)}/>
+                <i></i>
+                <span>{this.props.children}</span>
+            </label>
+        )
+    }
+}
+
+Radio.defaultProps = {
+    style: {},
+    disabled: false,
+    onChange: ()=> {
+    }
+}
