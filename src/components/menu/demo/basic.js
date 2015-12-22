102aa70c3f7f626841d70eddb4a94f94acc2e21b
huangziyi01 <huangziyi01@baidu.com>
2015-12-22 19:13:55 +0800

    quick push

--- src/components/menu/demo/basic.js
+++ src/components/menu/demo/basic.js
@@ -4,7 +4,7 @@ import { Menu, SubMenu, MenuItem } from 'tb-menu'
 export default class Demo extends React.Component {
     render() {
         return (
-            <Menu>
+            <Menu style={{height:100}}>
                 <MenuItem brand style={{width:100}}>管理系统</MenuItem>
                 <MenuItem>菜单项</MenuItem>
                 <MenuItem>菜单项</MenuItem>
