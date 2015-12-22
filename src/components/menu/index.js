dd3f3e5731954ecf38516a0e73defc474b58e97f
huangziyi01 <huangziyi01@baidu.com>
2015-12-22 16:08:14 +0800

    quick push

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -13,6 +13,14 @@ import Basic from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!./dem
 import basicCode from 'text!./demo/basic.js'
 import basicMarkdown from './demo/basic.md'
 
+import Reverse from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!./demo/reverse.js'
+import reverseCode from 'text!./demo/reverse.js'
+import reverseMarkdown from './demo/reverse.md'
+
+import Vertical from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!./demo/vertical.js'
+import verticalCode from 'text!./demo/vertical.js'
+import verticalMarkdown from './demo/vertical.md'
+
 export default class Menu extends React.Component {
     constructor(props) {
         super(props)
@@ -26,9 +34,17 @@ export default class Menu extends React.Component {
                 <Title>{readme}</Title>
 
                 <Row>
-                    <Col span="20" style={colStyle}>
+                    <Col span="24" style={colStyle}>
                         <CodeView md={basicMarkdown} code={basicCode}>
                             <Basic />
+                        </CodeView>
+
+                        <CodeView style={{marginTop:10}} md={reverseMarkdown} code={reverseCode}>
+                            <Reverse />
+                        </CodeView>
+
+                        <CodeView style={{marginTop:10}} md={verticalMarkdown} code={verticalCode}>
+                            <Vertical />
                         </CodeView>
                     </Col>
                 </Row>
