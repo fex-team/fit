4dc7f28bda3d4bfe878ee06cc2e8ecda67eb77c1
huangziyi01 <huangziyi01@baidu.com>
2016-01-25 13:36:22 +0800

    quick push

--- src/components/mobile/chat/demo/basic.js
+++ src/components/mobile/chat/demo/basic.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Chat from 'fiten-chat'
 
 export default class Demo extends React.Component {
+    handleNameRender(){
+        return (
+            <div>fitenRender</div>
+        )
+    }
+
     render() {
         return (
             <div>
@@ -10,6 +16,7 @@ export default class Demo extends React.Component {
                       portrait="http://himg.bdimg.com/sys/portrait/item/052ee28496e7bfb1e7be8ae7bebde5a4a7e7a9ba4620.jpg"/>
                 <Chat position="right"
                       name="fiten"
+                      nameRender={this.handleNameRender.bind(this)}
                       content="没错儿"
                       portrait="http://himg.bdimg.com/sys/portrait/item/052ee28496e7bfb1e7be8ae7bebde5a4a7e7a9ba4620.jpg"/>
             </div>
