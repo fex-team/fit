119103791e55b1f6376014eba323fd20c236adf0
huangqi03 <huangqi03@baidu.com>
2016-01-28 23:23:09 +0800

    Squashed 'lib/pc/upload/' changes from 92bc5d9..6aa8b52
    6aa8b52 Merge commit 'b1b03452e42728b85c180ced133889930b31c4cd'
    b1b0345 Squashed 'lib/pc/upload/' changes from 3f98925..d735662
    d735662 Merge commit 'a3b5cffe84e9b74feacab4a432e997fce4cadbf2'
    a3b5cff Squashed 'lib/pc/upload/' changes from 4a421d6..3f98925
    3f98925 Merge branch 'master' of http://gitlab.baidu.com/tb-component/pc-upload
    bc07d68 Squashed 'lib/pc/upload/' content from commit 4a421d6
    2ebcb54 Merge commit 'bc07d6849cf9e9d3796c0189f704f2f0b60e22a7' as 'lib/pc/upload'
    4a421d6 del
    c36395a Merge remote-tracking branch 'origin/pc-upload/master'
    2edb5a8 quick push
    REVERT: 92bc5d9 update
    
    git-subtree-dir: lib/pc/upload
    git-subtree-split: 6aa8b52fdcaf78b58e802b3d7b094e245ce03559

---
+++ lib/upload-file-list.jsx
@@ -0,0 +1,128 @@
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
+
+var UploadFileList = (function (_React$Component) {
+    _inherits(UploadFileList, _React$Component);
+
+    function UploadFileList() {
+        var _Object$getPrototypeO;
+
+        _classCallCheck(this, UploadFileList);
+
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+        }
+
+        var _this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(UploadFileList)).call.apply(_Object$getPrototypeO, [this].concat(args)));
+
+        _this.state = {};
+        return _this;
+    }
+
+    _createClass(UploadFileList, [{
+        key: 'textItemRender',
+        value: function textItemRender(info) {
+            var closeBtnStyle = {
+                fontSize: 12,
+                float: 'right',
+                marginLeft: 5,
+                color: '#98a6ad',
+                cursor: 'pointer'
+            };
+            if (this.props.type === 'picture') {
+                var picItemStyle = {
+                    padding: 8,
+                    border: '1px solid #edf1f2',
+                    margin: '5px 0'
+                };
+                var picItemImageStyle = {
+                    backgroundImage: 'url(' + info.url + ')',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    width: 40,
+                    height: 40
+                };
+                return React.createElement(
+                    'div',
+                    { key: info.name, style: picItemStyle },
+                    React.createElement(
+                        'div',
+                        { style: closeBtnStyle },
+                        'x'
+                    ),
+                    React.createElement(
+                        'div',
+                        { style: { display: 'table' } },
+                        React.createElement(
+                            'div',
+                            { style: {
+                                    display: 'table-cell',
+                                    border: '1px solid #edf1f2'
+                                } },
+                            React.createElement('div', { style: picItemImageStyle })
+                        ),
+                        React.createElement(
+                            'div',
+                            { style: {
+                                    display: 'table-cell',
+                                    verticalAlign: 'middle',
+                                    fontSize: 12,
+                                    padding: '0 5px'
+                                } },
+                            info.name
+                        )
+                    )
+                );
+            }
+            var textItemStyle = {
+                color: '#333',
+                fontSize: 12,
+                display: 'inline-block',
+                padding: '5px 8px 5px 8px',
+                backgroundColor: '#edf1f2',
+                borderRadius: 50,
+                margin: '10px 5px'
+            };
+            return React.createElement(
+                'div',
+                { style: textItemStyle, key: info.name },
+                React.createElement(
+                    'div',
+                    { style: closeBtnStyle },
+                    'x'
+                ),
+                info.name
+            );
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this2 = this;
+
+            return React.createElement(
+                'div',
+                null,
+                this.props.list.map(function (info) {
+                    return _this2.textItemRender(info);
+                })
+            );
+        }
+    }]);
+
+    return UploadFileList;
+})(React.Component);
+
+UploadFileList.defaultProps = {
+    type: 'text'
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = UploadFileList;
