03d523e5af6c80531dcb153f46c9343cb363921d
huangqi03 <huangqi03@baidu.com>
2016-01-28 20:40:53 +0800

    upload doc update

--- src/components/pc/upload/index.js
+++ src/components/pc/upload/index.js
@@ -12,6 +12,22 @@
                     import BasicCode from 'text!../../../../lib/pc/upload/demo/lists/basic.js'
                     import BasicMarkdown from '../../../../lib/pc/upload/demo/lists/basic.md'
                     
+                    import ButtonComponent from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!../../../../lib/pc/upload/demo/lists/button.js'
+                    import ButtonCode from 'text!../../../../lib/pc/upload/demo/lists/button.js'
+                    import ButtonMarkdown from '../../../../lib/pc/upload/demo/lists/button.md'
+                    
+                    import FilesComponent from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!../../../../lib/pc/upload/demo/lists/files.js'
+                    import FilesCode from 'text!../../../../lib/pc/upload/demo/lists/files.js'
+                    import FilesMarkdown from '../../../../lib/pc/upload/demo/lists/files.md'
+                    
+                    import DragComponent from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!../../../../lib/pc/upload/demo/lists/drag.js'
+                    import DragCode from 'text!../../../../lib/pc/upload/demo/lists/drag.js'
+                    import DragMarkdown from '../../../../lib/pc/upload/demo/lists/drag.md'
+                    
+                    import DemoComponent from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!../../../../lib/pc/upload/demo/lists/demo.js'
+                    import DemoCode from 'text!../../../../lib/pc/upload/demo/lists/demo.js'
+                    import DemoMarkdown from '../../../../lib/pc/upload/demo/lists/demo.md'
+                    
 
                 const colStyle = {
                     padding: 10
@@ -34,6 +50,30 @@
                     <Col span="12" style={colStyle}>
                         <CodeView md={BasicMarkdown} code={BasicCode}>
                             <BasicComponent/>
+                        </CodeView>
+                    </Col>
+                    
+                    <Col span="12" style={colStyle}>
+                        <CodeView md={ButtonMarkdown} code={ButtonCode}>
+                            <ButtonComponent/>
+                        </CodeView>
+                    </Col>
+                    
+                    <Col span="12" style={colStyle}>
+                        <CodeView md={FilesMarkdown} code={FilesCode}>
+                            <FilesComponent/>
+                        </CodeView>
+                    </Col>
+                    
+                    <Col span="12" style={colStyle}>
+                        <CodeView md={DragMarkdown} code={DragCode}>
+                            <DragComponent/>
+                        </CodeView>
+                    </Col>
+                    
+                    <Col span="12" style={colStyle}>
+                        <CodeView md={DemoMarkdown} code={DemoCode}>
+                            <DemoComponent/>
                         </CodeView>
                     </Col>
                     
