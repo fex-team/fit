96f06bdef6db614c181ee3436a4def22bc9a7953
huangziyi01 <huangziyi01@baidu.com>
2016-02-17 15:27:40 +0800

    quick push

--- scripts/module-manage/push.js
+++ scripts/module-manage/push.js
@@ -1,6 +1,10 @@
 import { execSync } from 'child_process'
 
 const push = (dirPath, moduleName, gitlabPrefix)=> {
+    // 删除 lib目录
+
+    // 删除 node_modules目录
+
     // push一下
     try {
         execSync(`cd lib/${dirPath}/${moduleName};git add -A;git commit -m "quick push"; git push`)
