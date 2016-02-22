7915e27dd032a54ec3e0c24ed086a44747a0181e
dongtiancheng <dongtiancheng@baidu.com>
2016-02-22 11:44:27 +0800

    quick push

--- scripts/cli/feature/publish.js
+++ scripts/cli/feature/publish.js
@@ -1,9 +1,18 @@
 import multiProcessor from '../utils/multi-processor'
-import { spawn } from 'child_process'
+import { spawn, execSync } from 'child_process'
+
 
 var root = process.cwd();
 
 export default function publishModules(modules) {
+
+	var whoami = execSync('npm whoami')
+
+	if (whoami !== 'tieba') {
+		console.log('you are not logined by tieba');
+		process.exit(1);
+	}
+
 	return multiProcessor(() => {
 		return spawn('npm', ['publish'])
 	}, (job) => {
