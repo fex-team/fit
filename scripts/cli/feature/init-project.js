f56c13ee5bb4363674ce4ec487c06724934c9d29
dongtiancheng <dongtiancheng@baidu.com>
2016-02-25 19:39:04 +0800

    quick push

--- scripts/cli/feature/init-project.js
+++ scripts/cli/feature/init-project.js
@@ -5,44 +5,40 @@ import conch from 'conch'
 import { execSync } from 'child_process'
 import _ from 'lodash'
 import { existsSync, mkdir, writeFileSync} from 'fs'
-import { setData, logError } from '../utils/summary'
-import { getRelativePath } from '../utils/util'
 
 export default function initProject (modules) {
 	modules.forEach((val) => {
-		let splits = val.split('/')
-		let moduleName = splits.pop()
-		let moduleType = splits.pop()
-		let componentJSON = getAllComponentJSON()
-		let gitlabPrefix = componentJSON.categorys[moduleType].gitlabPrefix
-		let prefix = componentJSON.categorys[moduleType].prefix
-		let description;
+				let splits = val.split('/')
+				let moduleName = splits.pop()
+				let moduleType = splits.pop()
+				let componentJSON = getAllComponentJSON()
+				let gitlabPrefix = componentJSON.categorys[moduleType].gitlabPrefix
+				let prefix = componentJSON.categorys[moduleType].prefix
+				let description;
 
-		let repoPath
-		if (gitlabPrefix !== '') {
-			repoPath = `http://gitlab.baidu.com/tb-component/${gitlabPrefix}-${moduleName}.git`
-		}
-		else {
-			repoPath = `http://gitlab.baidu.com/tb-component/${moduleName}.git`
-		}
-
-		_.each(componentJSON.categorys[moduleType].components, (category) => {
-			_.each(category, (item) => {
-				if (item.path === moduleName) {
-					description = item.name;
+				let repoPath
+				if (gitlabPrefix !== '') {
+					repoPath = `http://gitlab.baidu.com/tb-component/${gitlabPrefix}-${moduleName}.git`
 				}
-			})
-		})
+				else {
+					repoPath = `http://gitlab.baidu.com/tb-component/${moduleName}.git`
+				}
 
-		try {
+				_.each(componentJSON.categorys[moduleType].components, (category) => {
+					_.each(category, (item) => {
+						if (item.path === moduleName) {
+							description = item.name;
+						}
+					})
+				})
 
 
-			if (!description) {
-				execSync(`rm -rf ${val}`)
-				throw new Error(`can not create lib/${moduleType}/${moduleName}`)
-			}
+				if (!description) {
+					execSync(`rm -rf ${val}`)
+					throw new Error(`can not create lib/${moduleType}/${moduleName}`)
+				}
 
-			var templateJSON = `
+				var templateJSON = `
 				{
 				  "name": "${prefix}-${moduleName}",
 				  "version": "1.0.0",
@@ -65,7 +61,7 @@ export default function initProject (modules) {
 				}
 			`
 
-			var markdown = `
+				var markdown = `
 				# ${description}
 
 				---
@@ -76,41 +72,37 @@ export default function initProject (modules) {
 			`
 
 
-			if (!existsSync(path.join(val, 'package.json'))) {
-				console.log('INFO: add package.json for ' + val)
-				writeFileSync(path.join(val, 'package.json'), templateJSON)
-			}
+				if (!existsSync(path.join(val, 'package.json'))) {
+					console.log('INFO: add package.json for ' + val)
+					writeFileSync(path.join(val, 'package.json'), templateJSON)
+				}
 
-			if (!existsSync(path.join(val, 'readme.md')) || !existsSync(path.join(val, 'README.md'))) {
-				console.log('INFO: add readme.md for ' + val)
-				writeFileSync(path.join(val, 'readme.md'), conch(markdown).replace('\t', ''))
-			}
+				if (!existsSync(path.join(val, 'readme.md')) || !existsSync(path.join(val, 'README.md'))) {
+					console.log('INFO: add readme.md for ' + val)
+					writeFileSync(path.join(val, 'readme.md'), conch(markdown).replace('\t', ''))
+				}
 
-			if (!existsSync(path.join(val, 'demo'))) {
-				console.log('INFO: add demo for ' + val)
-				mkdir(path.join(val, 'demo'))
-			}
+				if (!existsSync(path.join(val, 'demo'))) {
+					console.log('INFO: add demo for ' + val)
+					mkdir(path.join(val, 'demo'))
+				}
 
-			if (!existsSync(path.join(val, 'src'))) {
-				console.log('INFO: add src directory for', val)
-				mkdir(path.join(val, 'src'))
-			}
+				if (!existsSync(path.join(val, 'src'))) {
+					console.log('INFO: add src directory for', val)
+					mkdir(path.join(val, 'src'))
+				}
 
-			if (!existsSync(path.join(val, 'src', 'index.js'))) {
-				console.log('INFO: add src/index.js for ', val)
-				writeFileSync(path.join(val, 'src', 'index.js'), '')
-			}
+				if (!existsSync(path.join(val, 'src', 'index.js'))) {
+					console.log('INFO: add src/index.js for ', val)
+					writeFileSync(path.join(val, 'src', 'index.js'), '')
+				}
 
-			if (!existsSync(path.join(val, 'demo', 'index.js'))) {
-				console.log('INFO: add demo/index.js for', val)
-				writeFileSync(path.join(val, 'demo', 'index.js'), '')
-			}
+				if (!existsSync(path.join(val, 'demo', 'index.js'))) {
+					console.log('INFO: add demo/index.js for', val)
+					writeFileSync(path.join(val, 'demo', 'index.js'), '')
+				}
 
-			setData(getRelativePath(val), 'init-project', true)
-		}
-		catch (e) {
-			setData(getRelativePath(val), 'init-project', false)
-			logError(getRelativePath(val), e.toString())
-		}
-	})
+				setData(getRelativePath(val), 'init-project', true)
+			}
+	)
 }
