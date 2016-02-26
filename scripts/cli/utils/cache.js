28e5fea95853758b8e64b6919ceed01b862e6347
dongtiancheng <dongtiancheng@baidu.com>
2016-02-26 13:31:53 +0800

    quick push

--- scripts/cli/utils/cache.js
+++ scripts/cli/utils/cache.js
@@ -1,6 +1,41 @@
-import fs from 'fs'
+import { existsSync, mkdirSync, writeFileSync, readFileSync} from 'fs'
+import path from 'path'
+import getProjectState from '../feature/git-status'
+import moduleDistribute from './distribute'
 
 function getUserHome() {
 	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
+function getCacheDir () {
+	let home = getUserHome()
+
+	if (!existsSync(home, '.awesome')) {
+		mkdirSync(home, '.awesome')
+	}
+
+	return path.join(home, '.awesome')
+}
+
+export function writeCache () {
+	let dir = getCacheDir()
+	let diff = moduleDistribute(getProjectState)
+
+	writeFileSync(path.join(dir, 'diff.txt'), diff.join('\n'))
+}
+
+export function getCache () {
+	let dir = getCacheDir()
+
+	if (!existsSync(path.join(dir, 'diff.txt'))) {
+		return []
+	}
+
+	return readFileSync(path.join(dir, 'diff.txt')).split('\n')
+}
+
+export function clearCache () {
+	let dir = getCacheDir()
+
+	writeFileSync(path.join())
+}
