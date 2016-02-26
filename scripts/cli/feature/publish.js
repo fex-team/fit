5de9a4a76cc04e279e9a5102b2f10fd66165bd96
dongtiancheng <dongtiancheng@baidu.com>
2016-02-26 17:43:16 +0800

    quick push

--- scripts/cli/feature/publish.js
+++ scripts/cli/feature/publish.js
@@ -15,7 +15,7 @@ export default function publishModules(modules) {
 	}
 
 	return multiProcessor(() => {
-		return spawn('npm', ['publish', '&>/dev/null', ' & ', ' > ', ' /dev/null'])
+		return spawn('npm', ['publish'])
 	}, (job) => {
 		if (/\/lib\/tb\/[a-z\-A-Z]+/.test(job.replace(root, ''))) {
 			return false
