bd546212bae791e00838ca105bef27e72e1c31f0
huangziyi01 <huangziyi01@baidu.com>
2016-03-17 10:07:15 +0800

    quick push

--- resolve.js
+++ resolve.js
@@ -38,6 +38,8 @@
                 
                 'fit-scroll-listen': path.join(__dirname, 'lib/pc/scroll-listen/src'),
                 
+                'fit-tooltip': path.join(__dirname, 'lib/pc/tooltip/src'),
+                
                 'fit-input': path.join(__dirname, 'lib/pc/input/src'),
                 
                 'fit-number': path.join(__dirname, 'lib/pc/number/src'),
