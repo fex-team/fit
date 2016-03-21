893d0f5eb6666226b416b5ae2c99e0f25b21124b
huangziyi01 <huangziyi01@baidu.com>
2016-03-21 17:01:59 +0800

    quick push

--- src/components/pc/progress/index.js
+++ src/components/pc/progress/index.js
@@ -23,6 +23,10 @@
                     import BasicCode from 'text!../../../../lib/pc/progress/demo/lists/basic.js'
                     import BasicMarkdown from '../../../../lib/pc/progress/demo/lists/basic.md'
                     
+                    import StatusComponent from 'react-hot-loader!babel?presets[]=react,presets[]=es2015!../../../../lib/pc/progress/demo/lists/status.js'
+                    import StatusCode from 'text!../../../../lib/pc/progress/demo/lists/status.js'
+                    import StatusMarkdown from '../../../../lib/pc/progress/demo/lists/status.md'
+                    
 
                 const colStyle = {
                     padding: 10
@@ -62,6 +66,16 @@
                                       code={BasicCode}>
 
                                     <BasicComponent/>
+
+                            </CodeView>
+                        </Col>
+                    
+                        <Col span="24" style={colStyle}>
+                            <CodeView store={store}
+                                      md={StatusMarkdown}
+                                      code={StatusCode}>
+
+                                    <StatusComponent/>
 
                             </CodeView>
                         </Col>
