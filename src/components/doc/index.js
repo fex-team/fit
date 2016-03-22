0da619ecdffa97d441a94b1da6142dd67d82b286
huangziyi01 <huangziyi01@baidu.com>
2016-03-22 15:26:55 +0800

    quick push

--- src/components/doc/index.js
+++ src/components/doc/index.js
@@ -26,8 +26,9 @@ export default class Doc extends React.Component {
                                    className="video-box"
                                    preload="auto"
                                    height="500">
-                                <source id="mp4"
-                                        src="http://agroup.baidu.com:8964/static/0c/e432642f982e7643559cd1a707e6086b53371e.mp4?filename=fit-learning.mp4"
+                                <source src="http://agroup.baidu.com:8964/static/5b/c2327af7daaec6b1f8888bcf1ea8007613261d.webm?filename=fit-learning.webm"
+                                        type="video/webm"/>
+                                <source src="http://agroup.baidu.com:8964/static/0c/e432642f982e7643559cd1a707e6086b53371e.mp4?filename=fit-learning.mp4"
                                         type="video/mp4"/>
                             </video>
                         </div>
