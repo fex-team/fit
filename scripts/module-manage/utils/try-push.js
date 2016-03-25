c80f2101f66e69929f6dbc434ec1754893b5e40f
huangziyi01 <huangziyi01@baidu.com>
2016-03-26 00:16:01 +0800

    quick push

--- scripts/module-manage/utils/try-push.js
+++ scripts/module-manage/utils/try-push.js
@@ -3,7 +3,7 @@ import consoleLog from './console-log'
 
 const tryPush = (path)=> {
     try {
-        execSync(`cd ${path};git add -A;git commit -m "quick push"`)
+        execSync(`cd ${path};git add -A;git commit -m "quick push";git push origin master`)
         return true
     } catch (e) {
         const errorString = e.toString()
