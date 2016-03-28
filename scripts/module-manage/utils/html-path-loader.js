9bb625e228bf1d38f8c9a50500b503547d1d1b72
huangziyi01 <huangziyi01@baidu.com>
2016-03-28 16:07:23 +0800

    quick push

--- scripts/module-manage/utils/html-path-loader.js
+++ scripts/module-manage/utils/html-path-loader.js
@@ -9,13 +9,22 @@ const parsePath = (filePath, info)=> {
     filePathArray.shift()
     filePathArray.shift()
 
-    console.log(filePathArray)
+    // 如果第一个元素就是 .js ,说明是根路径
+    const prefix = `fit-${info.categoryName}-${info.module.path}`
+    if (filePathArray[0].indexOf('.js') > -1) {
+        return prefix
+    } else {
+        filePathArray.pop()
+        const addonPath = filePathArray.join('-')
+        return `${prefix}-${addonPath}`
+    }
 }
 
 export default (filePath, info) => {
     let source = fs.readFileSync(filePath).toString()
 
     const name = parsePath(filePath, info)
+    console.log(name)
     source = source.replace(/_namespace/g, `fit-${info.categoryName}-${info.module.path}`)
 
     fs.writeFileSync(filePath, source)
