58eb1432ae6849014b01363c057f4f6e70a07641
huangziyi01 <huangziyi01@baidu.com>
2016-04-19 19:49:40 +0800

    quick push

--- scripts/module-manage/push.js
+++ scripts/module-manage/push.js
@@ -93,7 +93,7 @@ const deleteJSXAndJs = (info)=> {
 
     // 如果入口文件是 tsx,再把 .js 文件删除
     if (!fs.existsSync(`${modulePath}/index.tsx`)) {
-        console.log(modulePath)
+        console.log('删除js', modulePath)
         //execSync(`find ${modulePath} -name "*.js" | xargs rm`)
     }
 }
