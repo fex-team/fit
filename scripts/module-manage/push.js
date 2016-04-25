3d9119e96186e47d4ed5b95a59adad4ed6b044e8
huangziyi01 <huangziyi01@baidu.com>
2016-04-25 19:00:59 +0800

    quick push

--- scripts/module-manage/push.js
+++ scripts/module-manage/push.js
@@ -36,7 +36,7 @@ const deleteLib = (info)=> {
 const createDTs = (info)=> {
     const tsxPath = `./lib/${info.categoryName}/${info.module.path}/src/index.tsx`
     if (fs.existsSync(tsxPath)) {
-        execSync(`tsc -d --experimentalDecorators --jsx preserve --t es6 ${tsxPath}`)
+        execSync(`tsc -d --experimentalDecorators --jsx preserve --t es6 -m commonjs ${tsxPath}`)
     }
 }
 
