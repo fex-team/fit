9925f26c9f2933a7253257b0afb59900d2cbd2f0
huangziyi01 <huangziyi01@baidu.com>
2016-04-28 15:46:12 +0800

    quick push

--- scripts/module-manage/push.js
+++ scripts/module-manage/push.js
@@ -61,8 +61,11 @@ const fitDts = (content, info, filePath)=> {
     content.replace(/import\s+\'[.\/\w-]+.(scss\';)$/g, '')
 
     // 所有相对定位引用,改为绝对定位引用
-    content.replace(/import\s+\*\s+as\s+\w+\s+from\s+\'([.\/\w-]+)\';|import\s+\w+\s+from\s+\'([.\/\w-]+)\';/g, (match, match1, match2)=> {
-        console.log(match, match1, match2)
+    content.replace(/import\s+\*\s+as\s+(\w+)\s+from\s+\'([.\/\w-]+)\';/g, (match, match1, match2)=> {
+        console.log(match1,match2)
+    })
+    content.replace(/import\s+(\w+)\s+from\s+\'([.\/\w-]+)\';/g, (match, match1, match2)=> {
+        console.log(match1,match2)
     })
 
     return content
