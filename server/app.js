bb0607e6a6c60037095840208cd067a9bdf48750
huangziyi01 <huangziyi01@baidu.com>
2016-05-04 17:56:06 +0800

    update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 
 var koa = require('koa')
 var app = koa()
-var getIp = require('./utils/get-ip')
+var getIp = require('request-ip')
 var checkBaidu = require('./utils/check-baidu')
 
 // 设置静态资源缓存
@@ -34,9 +34,9 @@ var args = process.argv.slice(2)
 
 var templateHtml = require('../html.js')(args)
 app.use(function *() {
-    var ip = getIp(this.req)
+    var ip = getIp.getClientIp(this.req)
     var isBaidu = checkBaidu(ip)
-
+    console.log('ip:', ip, isBaidu)
     templateHtml = templateHtml.replace(/__tplData\(\'isBaidu\'\)/g, isBaidu)
 
     this.type = 'text/html; charset=utf-8'
--- server/utils/get-ip/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function getClientIp(req) {
-    return req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress
-}
