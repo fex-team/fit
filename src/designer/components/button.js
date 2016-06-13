d9df8827444b520ace7e9cc1bd12dd1af372884c
huangziyi01 <huangziyi01@baidu.com>
2016-06-13 16:17:33 +0800

    quick push

--- src/designer/components/button.js
+++ src/designer/components/button.js
@@ -10,21 +10,69 @@ export default class ButtonComponent extends React.Component {
 }
 
 ButtonComponent.defaultProps = {
-    name     : '按钮',
+    name     : '自定义按钮',
     uniqueKey: 'button',
     icon     : 'square-o',
     options  : {
-        text: {
-            label   : '文字',
-            value   : '按钮',
+        text      : {
+            label   : '文字选项',
+            value   : '我是自定义按钮',
             editor  : 'text',
             editable: true
         },
-        id  : {
-            label   : 'id',
+        testSelect: {
+            label   : '下拉选项',
             value   : '5',
-            editor  : 'text',
-            editable: true
+            editor  : 'selector',
+            editable: true,
+            options : [{
+                key  : 'a',
+                value: '小明'
+            }, {
+                key  : 'b',
+                value: '小红'
+            }, {
+                key  : 'c',
+                value: '小白'
+            }, {
+                key  : 'd',
+                value: '小王'
+            }, {
+                key  : 'e',
+                value: '小李'
+            }, {
+                groupValue: '其它',
+                children  : [{
+                    key  : 'aa',
+                    value: '小黑'
+                }, {
+                    key  : 'bb',
+                    value: '小天'
+                }]
+            }]
+        },
+        array     : {
+            label   : '数组选项',
+            editor  : 'array',
+            children: {
+                name: {
+                    label   : '数组内选项1',
+                    editor  : 'text',
+                    editable: true
+                },
+                age : {
+                    label   : '数组内选项2',
+                    editor  : 'text',
+                    editable: true
+                }
+            },
+            value   : [{
+                name: '小明',
+                age : 16
+            }, {
+                name: '小王',
+                age : 18
+            }]
         }
     }
 }
