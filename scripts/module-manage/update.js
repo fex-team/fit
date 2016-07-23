1b1c645c62f188b8ec9c87dc1a8a7fdbf7841252
huangziyi01 <huangziyi01@baidu.com>
2016-07-23 22:47:57 +0800

    quick push

--- scripts/module-manage/update.js
+++ scripts/module-manage/update.js
@@ -40,6 +40,17 @@ const cloneModuleIfNotExist = (info)=> {
 
 const checkGitControl = (info)=> {
     const pathInfo = `lib/${info.categoryName}/${info.module.path}`
+
+    // 获得当前项目的git路径
+    let projectName = execSync(`cd ${pathInfo};git remote -v | head -n1 | awk '{print $2}' | sed -e 's,.*:\(.*/\)\?,,' -e 's/\.git$//'`).toString().trim()
+
+    let gitSourcePath = getGitSourcePath(info)
+    let expectModuleName = `${gitPlantform}/${gitPlantformGroup}/${gitSourcePath}`
+
+    console.log(projectName + '.git', expectModuleName)
+    if (projectName + '.git' !== expectModuleName) {
+        consoleLog(`错误:不要手动创建lib目录的任何文件夹,请在${gitPlantform}/${gitPlantformGroup}建立项目后,填写到all-component.json, 再重新执行npm update会自动创建,请删除此文件夹（删除前先做好备份）`, 'red', getModulePath(info))
+    }
 }
 
 export default (info)=> {
