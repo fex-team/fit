7e8709d6c3955ec98aa2915f2c08ce26fa7d6ef5
huangziyi01 <huangziyi01@baidu.com>
2016-08-03 17:01:11 +0800

    增加travis环境变量

--- scripts/module-manage/index.js
+++ scripts/module-manage/index.js
@@ -14,6 +14,7 @@ program
     .option('-u, --update', '更新')
     .option('-p, --push', '提交')
     .option('-t, --travis', '持续集成')
+    .option('-tk, --token', 'github的token')
     .option('-m, --message [value]', '提交时带的信息', 'quick push')
     .parse(process.argv)
 
