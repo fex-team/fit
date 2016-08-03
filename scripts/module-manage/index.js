b317600b1d578ec2d9ff69b28be755a8a958dbb2
huangziyi01 <huangziyi01@baidu.com>
2016-08-03 17:25:58 +0800

    删除注释

--- scripts/module-manage/index.js
+++ scripts/module-manage/index.js
@@ -35,43 +35,41 @@ const clearDts = ()=> {
     })
 }
 
-// if (program.update) {
-//     // 更新
-//     mapModule(config, (info)=> {
-//         // 如果是 travis 模式, 跳过内部组件
-//         if (program.travis && info.categoryInfo.access === 'private') {
-//             return
-//         }
-//
-//         update(info)
-//     })
-//     tryPull('./')
-// }
-//
-// if (program.push) {
-//     // 提交
-//     // 先清理一遍,防止编译到一半取消留下脏文件
-//     clearDts()
-//
-//     // 解析 import 语句,添加依赖
-//     upgradeDependencies(allModules)
-//
-//     // 更新版本依赖,对有修改的或者被依赖的发布新版本
-//     versionPatch(allModules)
-//
-//     mapModule(config, (info)=> {
-//         // 如果是 travis 模式, 跳过内部组件
-//         if (program.travis && info.categoryInfo.access === 'private') {
-//             return
-//         }
-//         // 组件提交（内含各种编译）
-//         push(info, program.message, program)
-//         // 清空所有 dts
-//         clearDts()
-//     })
-//
-//     // fit 项目提交（直接提交）
-//     tryPush('./', program.message)
-// }
+if (program.update) {
+    // 更新
+    mapModule(config, (info)=> {
+        // 如果是 travis 模式, 跳过内部组件
+        if (program.travis && info.categoryInfo.access === 'private') {
+            return
+        }
 
-console.log('判断travis:', program.travis)
+        update(info)
+    })
+    tryPull('./')
+}
+
+if (program.push) {
+    // 提交
+    // 先清理一遍,防止编译到一半取消留下脏文件
+    clearDts()
+
+    // 解析 import 语句,添加依赖
+    upgradeDependencies(allModules)
+
+    // 更新版本依赖,对有修改的或者被依赖的发布新版本
+    versionPatch(allModules)
+
+    mapModule(config, (info)=> {
+        // 如果是 travis 模式, 跳过内部组件
+        if (program.travis && info.categoryInfo.access === 'private') {
+            return
+        }
+        // 组件提交（内含各种编译）
+        push(info, program.message, program)
+        // 清空所有 dts
+        clearDts()
+    })
+
+    // fit 项目提交（直接提交）
+    tryPush('./', program.message)
+}
